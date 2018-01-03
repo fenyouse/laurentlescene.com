31c87b01b9fecec62fddf8efa4f5280775fde26c
Fenyouse <8859822+fenyouse@users.noreply.github.com>
2018-01-03 22:43:05 +0100

    commit

--- UtilJDR/index.php
+++ UtilJDR/index.php
@@ -2,6 +2,7 @@
 
   session_start();
   require_once 'vues/header.php';
+  require_once 'modeles/Mobile_Detect.php';
 
 	//référencer les classes utiles
 	require_once 'modeles/si.php';
@@ -10,6 +11,16 @@
 	//var_dump($MonSI);//montrer l'id de l'objet
 
   require_once 'controleurs/controleurPrincipal.php';
-
+  $detect = new Mobile_Detect;
+  if (!$detect->isMobile()) {
   require_once 'vues/footer.php';
+  }
 ?>
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
